d5dce326fe726453d91f71735a850440f3938435
nargok <nargok@gmail.com>
2024-05-19 17:46:59 +0900

    Edit Image

--- lib/edit_snap_screen.dart
+++ lib/edit_snap_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:edit_snap/gen/assets.gen.dart';
+import 'package:image/image.dart' as image_lib;
 
 class ImageEditScreen extends StatefulWidget {
   const ImageEditScreen({super.key, required this.imageBitmap});
@@ -14,27 +15,61 @@ class ImageEditScreen extends StatefulWidget {
 }
 
 class _ImageEditScreenState extends State<ImageEditScreen> {
+
+  late Uint8List _imageBitmap;
+
+  @override
+  void initState() {
+    super.initState();
+    _imageBitmap = widget.imageBitmap;
+  }
+
+  void _rotateImage() {
+    // 画像データをデコードする
+    final image = image_lib.decodeImage(_imageBitmap);
+    if (image == null) return;
+    final rotateImage = image_lib.copyRotate(image, angle: 90);
+    setState(() {
+      _imageBitmap = image_lib.encodeBmp(rotateImage);
+    });
+  }
+
+  void _flipImage() {
+    // 画像データをデコードする
+    final image = image_lib.decodeImage(_imageBitmap);
+    if (image == null) return;
+    final flipImage = image_lib.copyFlip(
+        image, direction: image_lib.FlipDirection.horizontal);
+
+    setState(() {
+      _imageBitmap = image_lib.encodeBmp(flipImage);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     final l10n = L10n.of(context);
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        backgroundColor: Theme
+            .of(context)
+            .colorScheme
+            .inversePrimary,
         title: Text(l10n.imageEditScreenTitle),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Image.memory(widget.imageBitmap),
+            Image.memory(_imageBitmap),
             IconButton(
-                onPressed: () {},
+                onPressed: () => _rotateImage(),
                 icon: Assets.rotateIcon.svg(
                   width: 24,
                   height: 24,
                 )),
             IconButton(
-                onPressed: () {},
+                onPressed: () => _flipImage(),
                 icon: Assets.flipIcon.svg(width: 24, height: 24)),
           ],
         ),
