915c44c562a54b6a1bcf6e0a36da8edf9ad48c58
nargok <nargok@gmail.com>
2024-05-19 17:03:39 +0900

    select image

--- lib/image_select_screen.dart
+++ lib/image_select_screen.dart
@@ -1,5 +1,8 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:image/image.dart' as image_lib;
 
 class ImageSelectScreen extends StatefulWidget {
   const ImageSelectScreen({super.key});
@@ -9,9 +12,49 @@ class ImageSelectScreen extends StatefulWidget {
 }
 
 class _ImageSelectScreenState extends State<ImageSelectScreen> {
+  final ImagePicker _picker = ImagePicker();
+  Uint8List? _imageBitmap;
+
+  Future<void> _selectImage() async {
+    // 画像を選択する
+    final XFile? imageFile =
+        await _picker.pickImage(source: ImageSource.gallery);
+
+    // ファイルオブジェクトから画像をデータを取得する
+    final imageBitmap = await imageFile?.readAsBytes();
+    assert(imageBitmap != null);
+    if (imageBitmap == null) return;
+
+    // 画像データをデコードする
+    final image = image_lib.decodeImage(imageBitmap);
+    assert(image != null);
+    if (image == null) return;
+
+    final image_lib.Image resizedImage;
+    if (image.width > image.height) {
+      // 横長画像なら横幅を500pxにする
+      resizedImage = image_lib.copyResize(image, width: 500);
+    } else {
+      // 縦長画像なら縦幅を500pxにする
+      resizedImage = image_lib.copyResize(image, height: 500);
+    }
+
+    setState(() {
+      _imageBitmap = image_lib.encodeBmp(resizedImage);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     final l10n = L10n.of(context);
+    /**_imageBitmapはnull許容型。
+     * 一時変数においてnullチェックをするとタイププロモーションが働き,
+     * 非null許容型のように扱える
+     * _imageBitmapを使うと↓のようにしないといけない
+     * if(_imageBitmap != null) Image.memory(_imageBitmap!),
+     */
+    final imageBitmap = _imageBitmap;
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
@@ -21,8 +64,11 @@ class _ImageSelectScreenState extends State<ImageSelectScreen> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            ElevatedButton(onPressed: () {}, child: Text(l10n.imageSelect)),
-            ElevatedButton(onPressed: () {}, child: Text(l10n.imageEdit)),
+            if (imageBitmap != null) Image.memory(imageBitmap!),
+            ElevatedButton(
+                onPressed: () => _selectImage(), child: Text(l10n.imageSelect)),
+            if (imageBitmap != null)
+              ElevatedButton(onPressed: () {}, child: Text(l10n.imageEdit)),
           ],
         ),
       ),
